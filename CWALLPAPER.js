db1e9d5b93d8de7e09e3ff297f05adf374892b20
Shadow-we <rakhaaqilahvicrie@gmail.com>
2026-06-29 19:32:12 +0700

    fix error js

--- CWALLPAPER.js
+++ CWALLPAPER.js
@@ -1,12 +1,41 @@
-let btn_database = document.querySelector('#database');
+// link input
 let btn_live_walllpaper = document.querySelector('#submit-live-wallpaper'); 
-let live_wallpaper = document.getElementById('input-link');
+let inputWallpaper = document.getElementById('input-link');
+// database
+let btn_database = document.querySelector('#database-button');
+let list_database = document.querySelector('#database');
+
+function menambahGambar() {
+    const gambar = inputWallpaper.value.trim();
+
+    if (gambar) {
+        buatGambarElement(gambar);
+        inputWallpaper.value = ' ';
+    } else {
+        alert("please enter url");
+    }
+}
+
+function  buatGambarElement(gambar) {
+    const listGambar = document.createElement('li');
+    const tagGambar = document.createElement('img'); 
+
+    tagGambar.src = gambar;                       
+    tagGambar.alt = "Wallpaper";                     
+    tagGambar.style.width = "100px";                 
+
+    listGambar.appendChild(tagGambar);               
+    list_database.appendChild(listGambar);           
+}
 
 btn_live_walllpaper.addEventListener('click',() => {
-    let imageUrl = live_wallpaper.value;
+    let imageUrl = inputWallpaper.value;
     if (imageUrl) {
         document.body.style.backgroundImage = `url('${imageUrl}')`;
     } else {
         alert("cari walpaper terlebih dahulu")
     }
 });
+
+btn_database.addEventListener('click', menambahGambar)
+
