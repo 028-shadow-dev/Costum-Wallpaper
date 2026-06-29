0e893896323c1731e948852bfb06cdb28a23ff92
Shadow-we <rakhaaqilahvicrie@gmail.com>
2026-06-29 13:11:00 +0700

    wallpaper costum html css js(no database)

---
+++ CWALLPAPER.js
@@ -0,0 +1,12 @@
+let btn_database = document.querySelector('#database');
+let btn_live_walllpaper = document.querySelector('#submit-live-wallpaper'); 
+let live_wallpaper = document.getElementById('input-link');
+
+btn_live_walllpaper.addEventListener('click',() => {
+    let imageUrl = live_wallpaper.value;
+    if (imageUrl) {
+        document.body.style.backgroundImage = `url('${imageUrl}')`;
+    } else {
+        alert("cari walpaper terlebih dahulu")
+    }
+})
