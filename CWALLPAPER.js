fda271d56c9b92edd25db14311048e52b6fbc524
Shadow-we <rakhaaqilahvicrie@gmail.com>
2026-06-30 13:16:57 +0700

    js:can store many link, css: minimalis

--- CWALLPAPER.js
+++ CWALLPAPER.js
@@ -4,8 +4,10 @@ const inputWallpaper = document.getElementById('input-link');
 // database
 const btn_database = document.querySelector('#database-button');
 const list_database = document.querySelector('#database');
+const search_database = document.getElementById('search-database');
 // setting
 const setting = document.getElementById('setting');
+
 
 setting.addEventListener('click', () => { 
     let visibleFooterSetting = document.getElementById('footerhide');
@@ -16,25 +18,42 @@ setting.addEventListener('click', () => {
     }
 });
 
+let keyNumbers = 0;
+btn_database.addEventListener('click', () => {
+    const linkSave = inputWallpaper.value;
+    keyNumbers += 1;
+    let newKey = `link_${keyNumbers}`;
+    localStorage.setItem(newKey, linkSave);
+    alert(`link sudah tersave sebagai no ${newKey}`);
+    return newKey;
+});
+
+
+// search_database.addEventListener('click', (newKey) => {
+//     const database = localStorage.getItem(link_1);
+//     let searchDatabase = prompt("pilih no database:");
+//         if (searchDatabase == database) {
+//             document.body.style.backgroundImage = `url('${searchDatabase}')`;
+//         } else if (searchDatabase == 0) {
+//             document.body.style.backgroundImage = `url('${searchDatabase}')`;
+//         }
+// });
+
+let bgNoRefresh = localStorage.getItem('link_1');
+if (bgNoRefresh) {
+    document.body.style.backgroundImage = `url('${bgNoRefresh}')`;
+}
+
 btn_live_walllpaper.addEventListener('click',() => {
     let linkGambar = inputWallpaper.value;
     if (linkGambar) {
         document.body.style.backgroundImage = `url('${linkGambar}')`;
-        return linkGambar;
     } else {
         alert("cari walpaper terlebih dahulu")
     }
 });
 
-let bgNoRefresh = localStorage.getItem('link');
-if (bgNoRefresh) {
-    document.body.style.backgroundImage = `url('${bgNoRefresh}')`;
-}
 
-btn_database.addEventListener('click', () => {
-    const linkSave = inputWallpaper.value;
-    localStorage.setItem('link', linkSave);
-})
 
 // Date (year, month, day, hour, minute, ms)
 function UpdateJam() {
@@ -42,6 +61,7 @@ function UpdateJam() {
 
     const jam = document.getElementById('jam-sekarang');
     const date = new Date();
+    date.toLocaleDateString('id-ID');
     const year = date.getFullYear();
     const monthN = date.getMonth();
     const day = date.getDate();
@@ -50,7 +70,7 @@ function UpdateJam() {
 
     const htmlKonten = `
         <h1>${hour}:${minute}</h1>
-        <h3>${year}, ${monthH[monthN]} ${day} </h3>
+        <h3>${year},${monthH[monthN]} ${day} </h3>
     `
 
 
