585568609e5967c237bbac902ca3a121bec89361
Shadow-we <rakhaaqilahvicrie@gmail.com>
2026-08-03 13:45:33 +0700

    JS: FIX shortcut

--- CWALLPAPER.js
+++ CWALLPAPER.js
@@ -88,8 +88,6 @@ btn_modecolor.addEventListener('click', () => {
     }
 })
 
-
-
 ToDoList.addEventListener('click', () => {
     let visibleToDoList = document.getElementById('list-pekerjaan');
     if (visibleToDoList.style.visibility === "hidden") {
@@ -205,17 +203,19 @@ function addShortcut() {
         if (url === null) return; 
         if(name.trim() === "") {
             alert("Nama tidak boleh kosong!");
+            continue;
         }
         if(url.trim() === "") {
             alert("url tidak boleh kosong!");
+            continue;
         }
         break;
     }
     
-    
     if(!url.startsWith('http')) url = 'https://' + url; 
     const shortcuts = JSON.parse(localStorage.getItem('myShortcuts')) || [];
-    shortcuts.push({ name, url });
+    let key = Date.now();
+    shortcuts.push({ name, url, key});
     localStorage.setItem('myShortcuts', JSON.stringify(shortcuts));
     displayShortcuts();
 }
@@ -228,27 +228,27 @@ function displayShortcuts() {
     shortcuts.forEach((item, index) => {
         container.innerHTML += `
         <br>
-        <!-- Ubah id menjadi class agar tidak duplikat -->
         <div class="shortcut-item" style="border-radius: 10px; display: flex; align-items: center; gap: 0.5rem; text-decoration: none; color: black; border: 1px solid black; background: white; padding: 0.5rem;">
             <a id="shortcut-link-${index}" href="${item.url}" target="_blank" rel="noopener noreferrer" style="margin: 0; padding: 0.5rem; color: black; text-decoration: none;">${item.name}</a>
             
-            <!-- Kirim item.name langsung ke dalam fungsi editshortcut -->
-            <button onclick="editshortcut('${item.name}')" type="button" style="padding: 0.3rem 0.5rem;">Hapus</button>
+            <button onclick="editshortcut(${item.key}, '${item.name}')" type="button" style="padding: 0.3rem 0.5rem;">Hapus</button>
         </div>
         `;
     });
 
 }
 
-function editshortcut(name) {
+function editshortcut(key, name) {
     const yakin = confirm(`Apakah Anda yakin ingin menghapus shortcut "${name}"?`);
-    if (!name) return alert("Nama tidak boleh kosong!");
     const indexShortcut = JSON.parse(localStorage.getItem('myShortcuts')) || [];
 
-    let dataBaru = indexShortcut.filter(user => user.name.toLowerCase() !== name.toLowerCase());
+    if (!yakin) {
+        return;
+    }
+    let dataBaru = indexShortcut.filter(user => user.key !== key);
     localStorage.setItem("myShortcuts", JSON.stringify(dataBaru));
 
-    location.reload(); 
+    displayShortcuts()
 }
 
 function UpdateJam() {
